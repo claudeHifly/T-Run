10c9ddb678bc2b0c72247d75e1abb54c0f323401
Claudio Ciavola <44809571+doddo22@users.noreply.github.com>
2018-11-12 19:04:42 +0100

    suolo in basso

--- src/main_package/UserInterface.java
+++ src/main_package/UserInterface.java
@@ -12,32 +12,14 @@ import java.awt.*;
  *
  * @author claud
  */
+
 public class UserInterface extends JFrame {
 
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    public final int width = (int)(screenSize.getWidth()*0.9);
-    public final int height = (int)(screenSize.getHeight()*0.7);
-    
-    /*public double width = screenSize.getWidth();
-    public double height = screenSize.getHeight();*/
-    
-    @Override
-    public int getWidth(){
-        return (this.width);
-    }
-    
-    @Override
-    public int getHeight(){
-        return (this.height);
-    }
+    public static int width = (int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()*0.9);
+    public static int height = (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()*0.7);
     
     public UserInterface() {
 
-        /*
-        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-        width = (int)(screenSize.getWidth()*0.9);
-        height = (int)(screenSize.getHeight()*0.7);*/
         
         add(new Board());//con questo metodo inserisco una Board al centro del contenitore JFrame
         setTitle("T-Run");//setto il titolo
@@ -45,21 +27,17 @@ public class UserInterface extends JFrame {
 
         setSize(width, height);
         setLocationRelativeTo(null);//in questo modo posiziono il JFrame al centro dello schermo
-        setVisible(true);//setto JFrame visibile
         setResizable(false);//dimensioni JFrame non modificabili
     }
 
     public static void main(String[] args) {
    
-        /*
-        javax.swing.SwingUtilities.invokeLater(() -> {
+        
+        EventQueue.invokeLater(() -> {
             UserInterface gameInterface = new UserInterface();
-        });*/
+            gameInterface.setVisible(true);
+        });
         
-        UserInterface gameInterface = new UserInterface();
-        
-        System.out.println(gameInterface.width);
-        System.out.println(gameInterface.height);
     }
 
 }
