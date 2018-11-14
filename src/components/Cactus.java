515ad223315f6c88d163243f46da63f47712dfa3
sivoc <sivoccia.luca@gmail.com>
2018-11-14 17:05:48 +0100

    Collision areas

--- src/components/Cactus.java
+++ src/components/Cactus.java
@@ -5,8 +5,15 @@
  */
 package components;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.geom.Area;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
 import utility.*;
 
 
@@ -17,14 +24,25 @@ import utility.*;
 public class Cactus extends Obstacle{
     
     private Area collider;
+    private ImageOutline outline;
     
     public Cactus(int x, int y) {
         super(new Utility().create("src/image/Cactus-1.png"), x, y);
+        outline = new ImageOutline(super.getImage());
         collider = new Area();
-        collider.add(new Area(new Rectangle(super.getX(), super.getY(), super.getImage().getWidth(), super.getImage().getHeight())));
+        //collider.add(new Area(new Rectangle(super.getX(), super.getY(), super.getImage().getWidth(), super.getImage().getHeight())));
+        collider.add(new Area(outline.getOutline(super.getImage())));
     }
     
-
-
+    public static void main(String[] args) throws Exception {
+        Cactus cactus = new Cactus(0, 0);
         
+        JFrame f = new JFrame("Image Outline");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(cactus.outline.getGui());
+        f.pack();
+        f.setResizable(false);
+        f.setLocationByPlatform(true);
+        f.setVisible(true);
+    }
 }
