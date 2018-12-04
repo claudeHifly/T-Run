b08472c76e72994ffbc50543390375c6bcec8732
GennaroGem7 <gemito.gen@gmail.com>
2018-12-04 17:01:50 +0100

    TRex fix - devo ancora sistemare il salto quando auenta la velocità

--- src/components/TRex.java
+++ src/components/TRex.java
@@ -39,6 +39,7 @@ public class TRex extends KeyAdapter {
     public final static int groundLevel = (int) (UserInterface.height * 0.75);
     private final static int maxHeight = (int) (UserInterface.height - UserInterface.height * 0.52);
     private static int jumpFactor = (int) (movementSpeed * 1.3);
+    private static int TRexOnGround;
     public final static int x = 50;
     private int y;
     private int TRexPositionY;
@@ -48,22 +49,22 @@ public class TRex extends KeyAdapter {
     private static int wTRex;
     private static int hTRex;
     private boolean jumpDisabled;
-    
+
     private static int wTRexLower;
     private static int hTRexLower;
-    
+
     private Area collider;
     private int foot;
 
     private int topTRex;
     private int bottomTRex;
 
-    private final int   LEFT_FOOT = 1,
-                        RIGHT_FOOT = 2,
-                        NO_FOOT = 3,
-                        LEFT_FOOT_LOWER = 4,
-                        RIGHT_FOOT_RIGHT = 5;
-                                
+    private final int LEFT_FOOT = 1,
+            RIGHT_FOOT = 2,
+            NO_FOOT = 3,
+            LEFT_FOOT_LOWER = 4,
+            RIGHT_FOOT_RIGHT = 5;
+
 
     //stato T-Rex
     private static int state;
@@ -98,10 +99,12 @@ public class TRex extends KeyAdapter {
         hTRexLower = lowerHeadDinoLeft.getHeight(null);
         topTRex = (int) (Ground.yPosition) + (int) (Ground.yPosition * 0.025);
         bottomTRex = (int) (Ground.yPosition) + (int) (Ground.yPosition * 0.025) - hTRex;
-        
+
         TRexPositionY = bottomTRex;
 
-        y = (int) (Ground.yPosition) + (int) (Ground.yPosition * 0.025) - hTRex;
+        TRexOnGround = (int) (Ground.yPosition) + (int) (Ground.yPosition * 0.025) - hTRex;
+
+        y = TRexOnGround;
         System.out.println("TRex width: " + wTRex);
         System.out.println("TRex height: " + hTRex);
         System.out.println("Ground height: " + y);
@@ -118,57 +121,53 @@ public class TRex extends KeyAdapter {
 
     @Override
     public void keyPressed(KeyEvent e) {
-                
+
         int keyPressed = e.getKeyCode();
-     
-        if ( (keyPressed == KeyEvent.VK_SPACE || keyPressed == KeyEvent.VK_UP) && state != (LOWER_HEAD) && !(jumpDisabled)) {
+
+        if ((keyPressed == KeyEvent.VK_SPACE || keyPressed == KeyEvent.VK_UP) && state != (LOWER_HEAD) && !(jumpDisabled)) {
             state = JUMPING;
             jumpDisabled = true;
             //System.out.println("Space pressed");
         }
-             
-        if (keyPressed == KeyEvent.VK_DOWN && state!=(JUMPING)) {
+
+        if (keyPressed == KeyEvent.VK_DOWN && state != (JUMPING)) {
             state = LOWER_HEAD;
         }
 
     }
-    
+
     @Override
-    public void keyTyped(KeyEvent e){
+    public void keyTyped(KeyEvent e) {
     }
-    
+
     @Override
-    public void keyReleased(KeyEvent e){
-           
+    public void keyReleased(KeyEvent e) {
+
         int keyTyped = e.getKeyCode();
-             
+
         if (keyTyped == KeyEvent.VK_DOWN) {
             state = RUNNING;
         }
-        
-        if ( (keyTyped == KeyEvent.VK_SPACE || keyTyped == KeyEvent.VK_UP)) {
+
+        if ((keyTyped == KeyEvent.VK_SPACE || keyTyped == KeyEvent.VK_UP)) {
             jumpDisabled = false;
             //System.out.println("Space pressed");
         }
-                
+
     }
-    
+
   
 
     //create viene invocato
     public void create(Graphics g) {
-        ////////////////////g.drawImage(image, X, y, null); 
+         
         Graphics2D g2d = (Graphics2D) g;
-        //g2d.setColor(Color.red);
-        //g2d.draw(collider);
-        //g2d.setColor(Color.BLACK);
+        g2d.setColor(Color.red);
+        g2d.draw(collider);
+        g2d.setColor(Color.BLACK);
         switch (state) {
 
             case RUNNING:
-
-                if (y <= groundLevel - hTRex) {
-                    y += 2;
-                }
 
                 if (foot == NO_FOOT) {
                     foot = LEFT_FOOT;
@@ -185,48 +184,45 @@ public class TRex extends KeyAdapter {
             case JUMPING:
                 AffineTransform at = new AffineTransform();
 
-                if ( speedForJumping > 0 /*TRexPositionY > maxHeight*/ && topReached == false) {
-                    
+                if (speedForJumping > 0 /*TRexPositionY > maxHeight*/ && topReached == false) {
+
                     //jumping sprite
-                    TRexPositionY -= deltaT * speedForJumping ;
-                    speedForJumping -= (deltaT * gravity);
-                    g.drawImage(image, x, TRexPositionY, null);
-                    //collider
-                    at.translate(0, -jumpFactor);
+
+                    y -= deltaT * speedForJumping;
+                    g.drawImage(image, x, y, null);
+                    at.translate(0, -deltaT * speedForJumping);
                     collider.transform(at);
-                    
+                    speedForJumping -= (deltaT * gravity);
+
                     //System.out.println("bottomTRex height: " + bottomTRex);
                     //break;
                 } else if (speedForJumping <= 0 && topReached == false) {
                     topReached = true;
-                    g.drawImage(image, x, TRexPositionY, null);
+                    g.drawImage(image, x, y, null);
 
                     //System.out.println("SALTOOOOOOOOOOOOOOO " + bottomTRex);
                 } else if (topReached == true) {
-                    
-                    if (speedForJumping < 0)
+
+                    if (speedForJumping < 0) {
                         speedForJumping = 0;
-                    
-                    //System.out.println("TRex position y iniziale = " + TRexPositionY);
-                    //System.out.println("detlaT iniziale = " + deltaT);
-                    //System.out.println("speedForJumping iniziale = " + speedForJumping);
-                    
-                    TRexPositionY += deltaT * speedForJumping;
-                    speedForJumping += (deltaT * gravity);
-                    //System.out.println("TRex position y finale = " + TRexPositionY);
-                    //System.out.println("detlaT finale = " + deltaT);
-                    //System.out.println("speedForJumping finale = " + speedForJumping);
-                    
-                    g.drawImage(image, x, TRexPositionY, null);
-                    at.translate(0, jumpFactor);
+                    }
+
+                    y += deltaT * speedForJumping;
+                    g.drawImage(image, x, y, null);
+                    at.translate(0, deltaT * speedForJumping);
                     collider.transform(at);
+                    speedForJumping += (deltaT * gravity);
                    
-                } 
-                
-                if (TRexPositionY >= y-20 && topReached == true) {
+                }
+
+                if (y >= TRexOnGround - 20 && topReached == true) {
                     //System.out.println("ground " + bottomTRex);
-                    g.drawImage(image, x, TRexPositionY, null);
-                    
+                    y = TRexOnGround;
+                    g.drawImage(image, x, y, null);
+                    at = new AffineTransform();
+                    at.translate(0, -collider.getBounds().getY() + y);
+                    collider.transform(at);
+
                     topReached = false;
                     gravity = (float) 0.981;
                     speedForJumping = (float) (Ground.movementSpeed * 2.2);
@@ -234,10 +230,10 @@ public class TRex extends KeyAdapter {
                     break;
                 }
                 break;
-                
+
             case LOWER_HEAD:
                 int lowerCount = 0;
-                
+
                 if (foot == NO_FOOT) {
                     foot = LEFT_FOOT_LOWER;
                     g.drawImage(lowerHeadDinoLeft, x, y, null);
@@ -251,7 +247,7 @@ public class TRex extends KeyAdapter {
                 break;
                 
 
-        }
+       }
     }
     
 
