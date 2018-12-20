33f2c93514005bb5b45c54a518ecce5bc4b37f12
Claudio Ciavola <44809571+doddo22@users.noreply.github.com>
2018-12-20 12:28:03 +0100

    suond

---
+++ src/utility/Sound.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+import java.io.File;
+import javax.sound.sampled.*;
+
+/**
+ *
+ * @author Gennaro
+ */
+public class Sound {
+    
+   private Clip clip;
+   private String name;
+
+   public Sound(String filename) {
+
+      try {
+
+         name = filename;
+         File yourFile = new File(filename);
+         AudioInputStream stream;
+         AudioFormat format;
+         DataLine.Info info;
+
+         stream = AudioSystem.getAudioInputStream(yourFile);
+         format = stream.getFormat();
+         info = new DataLine.Info(Clip.class, format);
+         clip = (Clip) AudioSystem.getLine(info);
+         clip.open(stream);
+      } catch (Exception e) {
+         System.out.println("Error reading sound file: " + filename +".\n" + e.toString());
+      }
+   }
+
+   public void playSound() {
+       
+      if(clip == null ){
+         if(name != null){
+            System.out.println("Error playing " + name + "; file not initialized");
+         }
+         return;
+      }
+      clip.start();
+      //clip.loop(Clip.LOOP_CONTINUOUSLY);
+   }
+
+   public void stopSound() {
+      if(clip == null ){
+         if(name != null){
+            System.out.println("Error stopping " + name + "; file not initialized");
+         }
+         return;
+      }
+      clip.stop();
+   }
+
+   public void playSoundOnce() {
+      if(clip == null ){
+         if(name != null){
+            System.out.println("Error playing " + name + "; file not initialized");
+         }
+         return;
+      }
+      clip.start();
+      clip.loop(0);
+   }
+    
+}
