db33a351816e2fae9d4afdd9dc04824285f3ca9d
Claudio Ciavola <44809571+doddo22@users.noreply.github.com>
2018-12-20 11:00:05 +0100

    CAMBIO GRAFICA BVY LUCA

--- src/components/HealthBar.java
+++ src/components/HealthBar.java
@@ -70,5 +70,13 @@ public class HealthBar{ //Singleton
     public static void setInstance(HealthBar instance) {
         HealthBar.instance = instance;
     }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
     
 }
--- src/general/UserInterface.java
+++ src/general/UserInterface.java
@@ -26,7 +26,8 @@ public class UserInterface extends JFrame { //Singleton
 
         this.setSize(width, height);
         this.setLocationRelativeTo(null);//in questo modo posiziono il JFrame al centro dello schermo
-        this.setResizable(true);//dimensioni JFrame non modificabili
+        this.setResizable(false);//dimensioni JFrame non modificabili
+        
 
     }
 
